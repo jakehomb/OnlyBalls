d5541191ce1594124ea2aabcf212a02b6fbf0b10
jakehomb <31379925+jakehomb@users.noreply.github.com>
2021-09-12 21:01:51 -0400

    Initial repo dump. Dont fucking judge me
    I don't have to justify myself to you dickheads

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/jakehomb/OnlyBalls
+
+go 1.17
+
+require (
+	github.com/gofiber/fiber/v2 v2.18.0
+	github.com/golang-jwt/jwt/v4 v4.0.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.29.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+)
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func main() {
+	log.Println("[*] Starting Onlyballs Server...")
+
+	// Create the Fiber worker so we can add routes to it
+	app := fiber.New()
+
+	// Set up logging functionality
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
+
+	app.Get("/", IndexHandler)
+	app.Post("/login", LoginHandler)
+
+	// Run the service
+	log.Fatalln(app.Listen(":8008"))
+
+	log.Println("[*] Shutting Onlyballs Server down...")
+}
+
+func IndexHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
+func LoginHandler(c *fiber.Ctx) error {
+	user := c.FormValue("user")
+	pass := c.FormValue("pass")
+
+	// Throws Unauthorized error
+	if user != "john" || pass != "doe" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	// Create token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = "John Doe"
+	claims["admin"] = true
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token": t})
+}
